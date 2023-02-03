cbd18b699b8716adf9b29fbe2ddfe8dea94a4a30
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-02-04 03:17:31 +0900

    app/Header: change Link Title

--- app/Header.tsx
+++ app/Header.tsx
@@ -5,11 +5,11 @@ function Header() {
   return (
     <header className="p-4 text-slate-200 bg-slate-700 flex items-baseline gap-4">
       <Link href="/" className="text-3xl underline mr-4">
-        Hoge
+        HouseHolds
       </Link>
 
       <Link href="/households" className="text-2xl hover:text-red-700">
-        Create Household
+        Create
       </Link>
     </header>
   );
